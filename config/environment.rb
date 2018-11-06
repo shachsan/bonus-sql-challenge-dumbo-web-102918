26e6d450a6181da31fb39457d4259fe21e28dd20
SANJAY K SAI <shachsan@hotmail.com>
2018-11-06 19:11:35 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 require 'bundler'
 require 'SQLite3'
-require_relative ''
+require_relative '../'
 Bundler.require
 # Setup a DB connection here
 DB = {:conn => SQLite3::Database.new("db/daily_show.db")}
