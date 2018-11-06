93a433735abcd7993d5267cb2a50b11abc0eded1
SANJAY K SAI <shachsan@hotmail.com>
2018-11-06 18:37:45 +0000

    automatically backed up by learn

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,10 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'csv'
+require 'pry'
+
+# csv_text = File.read("../daily_show_guests.csv")
+csv_text = File.read("../daily_show_guests.csv")
+csv = CSV.parse(csv_text, :headers => true)
+
+binding.pry
+0
